95ce2aea1c41c36c286bf8016a0eaf612511d029
iagoiao <47525570+iagoiao@users.noreply.github.com>
2019-02-11 10:44:41 -0200

    Add files via upload
    Code that handles the low-level reading of a 4x4 keypad using MSP430FR5739

--- main.c
+++ main.c
@@ -1,66 +1,48 @@
-/*Function that writes a digit to a seven segment display
- *
+/*Function that reads a 4x4 keypad and return the pressed key as a char
+ *Code was made more flexible allowing changes at the pins to be made in a easy way
  */
-/*
-current config
-seg a P1.7
-seg	b P1.6
-seg c P3.7
-seg d P3.6
-seg e P3.5
-seg f P3.4
-seg g P2.2
-
-disp 1 en P2.1
-disp 2 en P2.6
-disp 3 en P2.5
-disp 4 en P2.0
-*/
-//Pin Macros to increase the flexibility of the code
-#define segAPin	    GPIO_PIN7
-#define segBPin		GPIO_PIN6
-#define segCPin		GPIO_PIN7
-#define segDPin		GPIO_PIN6
-#define segEPin		GPIO_PIN5
-#define segFPin		GPIO_PIN4
-#define segGPin		GPIO_PIN2
-
-#define segAPort    GPIO_PORT_P1
-#define segBPort	GPIO_PORT_P1
-#define segCPort	GPIO_PORT_P3
-#define segDPort	GPIO_PORT_P3
-#define segEPort	GPIO_PORT_P3
-#define segFPort	GPIO_PORT_P3
-#define segGPort	GPIO_PORT_P2
-
-#define Disp1EnablePin  GPIO_PIN1
-#define Disp2EnablePin	GPIO_PIN6
-#define Disp3EnablePin	GPIO_PIN5
-#define Disp4EnablePin	GPIO_PIN0
-
-#define Disp1EnablePort	GPIO_PORT_P2
-#define Disp2EnablePort	GPIO_PORT_P2
-#define Disp3EnablePort	GPIO_PORT_P2
-#define Disp4EnablePort	GPIO_PORT_P2
-
 
 #include "driverlib.h"
-void WriteDigitTo7Seg(int digit, uint8_t displaynum);
-void Init7SegPins();
+char KeyPadRead();
+//Defines for the pins used by the keypad
+#define KeyPadRow1Pin	GPIO_PIN1
+#define KeyPadRow2Pin	GPIO_PIN2
+#define KeyPadRow3Pin	GPIO_PIN0
+#define KeyPadRow4Pin	GPIO_PIN1
 
+#define KeyPadRow1Port	GPIO_PORT_P1
+#define KeyPadRow2Port	GPIO_PORT_P1
+#define KeyPadRow3Port	GPIO_PORT_P3
+#define KeyPadRow4Port	GPIO_PORT_P3
+
+
+
+#define KeyPadColumn1Pin	GPIO_PIN2
+#define KeyPadColumn2Pin	GPIO_PIN3
+#define KeyPadColumn3Pin	GPIO_PIN3
+#define KeyPadColumn4Pin	GPIO_PIN4
+
+#define KeyPadColumn1Port	GPIO_PORT_P3
+#define KeyPadColumn2Port	GPIO_PORT_P3
+#define KeyPadColumn3Port	GPIO_PORT_P1
+#define KeyPadColumn4Port	GPIO_PORT_P1
+
+
+uint8_t test=0;
 void main(void)
 {
 
     //Stop watchdog timer
     WDT_A_hold(WDT_A_BASE);
-    Init7SegPins();
+    delay(10);//Delay 1 seg
+
+
     while(1)
-	{
-    	WriteDigitTo7Seg(7, 1);
-		WriteDigitTo7Seg(24, 2);
-		WriteDigitTo7Seg(26, 3);
-		WriteDigitTo7Seg(13, 4);
-	}
+    {
+    	test= KeyPadRead();
+    	if(test=='1')
+    		test=1;
+    }
 
     __bis_SR_register(LPM4_bits + GIE);
 
@@ -68,264 +50,175 @@ void main(void)
     __no_operation();
 }
 //Functions
-void Init7SegPins()
+char KeyPadRead()
 {
-		//Set pins as outputs
-	  GPIO_setAsOutputPin(segAPort,segAPin);
-	  GPIO_setAsOutputPin(segBPort,segBPin);
-	  GPIO_setAsOutputPin(segCPort,segCPin);
-	  GPIO_setAsOutputPin(segDPort,segDPin);
-	  GPIO_setAsOutputPin(segEPort,segEPin);
-	  GPIO_setAsOutputPin(segFPort,segFPin);
-	  GPIO_setAsOutputPin(segGPort,segGPin);
 
-	  GPIO_setAsOutputPin(Disp1EnablePort,Disp1EnablePin);
-	  GPIO_setAsOutputPin(Disp2EnablePort,Disp2EnablePin);
-	  GPIO_setAsOutputPin(Disp3EnablePort,Disp3EnablePin);
-	  GPIO_setAsOutputPin(Disp4EnablePort,Disp4EnablePin);
+	/* Line 8 -	P1.1
+	 * Line 7 -	P1.2
+	 * Line 6 -	P3.0
+	 * Line 5 -	P3.1
+	 * Line 4 -	P3.2
+	 * Line 3 -	P3.3
+	 * Line 2 -	P1.3
+	 * Line 1 -	P1.4
+	 */
+	char pressedkey;
+    uint8_t IsShorted=0;
+	//Set row pins as outputs
 
+
+    GPIO_setAsOutputPin(KeyPadRow1Port,KeyPadRow1Pin);
+    GPIO_setAsOutputPin(KeyPadRow2Port,KeyPadRow2Pin);
+	GPIO_setAsOutputPin(KeyPadRow3Port,KeyPadRow3Pin);
+	GPIO_setAsOutputPin(KeyPadRow4Port,KeyPadRow4Pin);
+
+    //Set output to low on all of the first 4 pins
+    GPIO_setOutputLowOnPin(KeyPadRow1Port,KeyPadRow1Pin);
+    GPIO_setOutputLowOnPin(KeyPadRow2Port,KeyPadRow2Pin);
+    GPIO_setOutputLowOnPin(KeyPadRow3Port,KeyPadRow3Pin);
+    GPIO_setOutputLowOnPin(KeyPadRow4Port,KeyPadRow4Pin);
+
+
+    //Set the remaining 4 pins as inputs with pull down resistors
+    GPIO_setAsInputPinWithPullDownResistor(KeyPadColumn1Port,KeyPadColumn1Pin);
+    GPIO_setAsInputPinWithPullDownResistor(KeyPadColumn2Port,KeyPadColumn2Pin);
+    GPIO_setAsInputPinWithPullDownResistor(KeyPadColumn3Port,KeyPadColumn3Pin);
+    GPIO_setAsInputPinWithPullDownResistor(KeyPadColumn4Port,KeyPadColumn4Pin);
+
+    //Set output High on the first pin, If it is connected to one of the other pins that pin must read 1 otherwise it should read 0
+    GPIO_setOutputHighOnPin(KeyPadRow1Port,KeyPadRow1Pin);
+    //Check if any of the buttons of the first row is pressed
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn1Port,KeyPadColumn1Pin);
+    if(IsShorted==1)
+    {
+    	pressedkey='1';
+    	return pressedkey;
+    }
+
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn2Port,KeyPadColumn2Pin);
+    if(IsShorted==1)
+    {
+     	pressedkey='2';
+       	return pressedkey;
+    }
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn3Port,KeyPadColumn3Pin);
+    if(IsShorted==1)
+    {
+      	pressedkey='3';
+       	return pressedkey;
+    }
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn4Port,KeyPadColumn4Pin);
+    if(IsShorted==1)
+    {
+      	pressedkey='A';
+       	return pressedkey;
+    }
+    GPIO_setOutputLowOnPin(KeyPadRow1Port,KeyPadRow1Pin);
+    //Check if any of the buttons of the second row is pressed
+    GPIO_setOutputHighOnPin(KeyPadRow2Port,KeyPadRow2Pin);
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn1Port,KeyPadColumn1Pin);
+   	if(IsShorted==1)
+    {
+       	pressedkey='4';
+      	return pressedkey;
+    }
+
+   	IsShorted	=	GPIO_getInputPinValue(KeyPadColumn2Port,KeyPadColumn2Pin);
+   	if(IsShorted==1)
+    {
+    	pressedkey='5';
+      	return pressedkey;
+    }
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn3Port,KeyPadColumn3Pin);
+    if(IsShorted==1)
+    {
+    	pressedkey='6';
+       	return pressedkey;
+    }
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn4Port,KeyPadColumn4Pin);
+    if(IsShorted==1)
+    {
+      	pressedkey='B';
+       	return pressedkey;
+    }
+    GPIO_setOutputLowOnPin(KeyPadRow2Port,KeyPadRow2Pin);
+
+    //Check if any of the buttons of the third row is pressed
+    GPIO_setOutputHighOnPin(KeyPadRow3Port,KeyPadRow3Pin);
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn1Port,KeyPadColumn1Pin);
+   	if(IsShorted==1)
+    {
+       	pressedkey='7';
+      	return pressedkey;
+    }
+
+   	IsShorted	=	GPIO_getInputPinValue(KeyPadColumn2Port,KeyPadColumn2Pin);
+   	if(IsShorted==1)
+    {
+    	pressedkey='8';
+      	return pressedkey;
+    }
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn3Port,KeyPadColumn3Pin);
+    if(IsShorted==1)
+    {
+    	pressedkey='9';
+       	return pressedkey;
+    }
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn4Port,KeyPadColumn4Pin);
+    if(IsShorted==1)
+    {
+      	pressedkey='C';
+       	return pressedkey;
+    }
+    GPIO_setOutputLowOnPin(KeyPadRow3Port,KeyPadRow3Pin);
+
+    //Check if any of the buttons of the fourth row is pressed
+    GPIO_setOutputHighOnPin(KeyPadRow4Port,KeyPadRow4Pin);
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn1Port,KeyPadColumn1Pin);
+   	if(IsShorted==1)
+    {
+       	pressedkey='*';
+      	return pressedkey;
+    }
+
+   	IsShorted	=	GPIO_getInputPinValue(KeyPadColumn2Port,KeyPadColumn2Pin);
+   	if(IsShorted==1)
+    {
+    	pressedkey='0';
+      	return pressedkey;
+    }
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn3Port,KeyPadColumn3Pin);
+    if(IsShorted==1)
+    {
+    	pressedkey='#';
+       	return pressedkey;
+    }
+    IsShorted	=	GPIO_getInputPinValue(KeyPadColumn4Port,KeyPadColumn4Pin);
+    if(IsShorted==1)
+    {
+      	pressedkey='D';
+       	return pressedkey;
+    }
+    //If the code reach this point means that no key was pressed
+    pressedkey='X';
+    GPIO_setOutputHighOnPin(KeyPadRow4Port,KeyPadRow4Pin);
+    //Set all outputs to low
+    return pressedkey;
 
 }
-void WriteDigitTo7Seg(int digit, uint8_t displaynum)//Assumes common cathode displays, and assumes that the enable is active high
+
+void delay(int delayin100ms)
 {
-	//Disables all segments
-	GPIO_setOutputLowOnPin(Disp1EnablePort,Disp1EnablePin);
-	GPIO_setOutputLowOnPin(Disp2EnablePort,Disp2EnablePin);
-	GPIO_setOutputLowOnPin(Disp3EnablePort,Disp3EnablePin);
-	GPIO_setOutputLowOnPin(Disp4EnablePort,Disp4EnablePin);
 
-	switch(displaynum)
-	{
-	case 1 :
-		GPIO_setOutputHighOnPin(Disp1EnablePort,Disp1EnablePin);
+//    BCSCTL3 |= XCAP_3;					// xtal has 12.5 pF caps
+    TA1CCR0 = 1638*2*delayin100ms;					// f_xtal / 2 - 1 gives 2 Hz *2 = 1 second
+    TA1CTL = TASSEL_1 + MC_1 + TA1CTL;	                // ACLK + Up Mode + Clear timer
+    TA1CTL &= ~BIT0;					// reset TAIFG
+    while ((TA1CTL & BIT0) == 0)
+    { 		        // wait for timer to reach TACCR0
+    }
+} // delay
 
-		break;
-	case 2 :
-		GPIO_setOutputHighOnPin(Disp2EnablePort,Disp2EnablePin);
-
-		break;
-	case 3 :
-		GPIO_setOutputHighOnPin(Disp2EnablePort,Disp3EnablePin);
-
-		break;
-	case 4 :
-		GPIO_setOutputHighOnPin(Disp4EnablePort,Disp4EnablePin);
-
-		break;
-	}
-
-
-
-	//Clear all segments
-	GPIO_setOutputLowOnPin(segAPort,segAPin);
-	GPIO_setOutputLowOnPin(segBPort,segBPin);
-	GPIO_setOutputLowOnPin(segCPort,segCPin);
-	GPIO_setOutputLowOnPin(segDPort,segDPin);
-	GPIO_setOutputLowOnPin(segEPort,segEPin);
-	GPIO_setOutputLowOnPin(segFPort,segFPin);
-	GPIO_setOutputLowOnPin(segGPort,segGPin);
-
-
-
-	switch(digit)
-	{
-	case 0	://abcdef
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		break;
-	case 1	://bc
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		break;
-	case 2	://abdeg
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-		break;
-	case 3	://abcdg
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-		break;
-	case 4	://bcgf
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		break;
-	case 5	://acdgf
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		break;
-	case 6	://acdefg
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-
-		break;
-	case 7	://abc
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-
-		break;
-	case 8	://abcdefg	;
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-		break;
-	case 9	://abcdfg
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-		break;
-	case 10	:		//Shows A abcefg
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-	break;
-	case 11	:		//Shows b cdefg
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-		break;
-	case 12	:		//Shows C adef
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-
-		break;
-	case 13	:		//Shows d bcdeg
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-		break;
-	case 14	:		//Shows E adefg
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-		break;
-	case 15	:		//Shows F aefg
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-		break;
-	case 16	:		//Shows r aef
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-
-
-		break;
-	case 17	:		//Shows t defg
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-		break;
-	case 18	:		//Shows n ceg
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-		break;
-	case 19	:		//Shows L def
-	GPIO_setOutputHighOnPin(segDPort,segDPin);
-	GPIO_setOutputHighOnPin(segEPort,segEPin);
-	GPIO_setOutputHighOnPin(segFPort,segFPin);
-
-		break;
-	case 20	:		//Shows U bcdef
-	GPIO_setOutputHighOnPin(segBPort,segBPin);
-	GPIO_setOutputHighOnPin(segCPort,segCPin);
-	GPIO_setOutputHighOnPin(segDPort,segDPin);
-	GPIO_setOutputHighOnPin(segEPort,segEPin);
-	GPIO_setOutputHighOnPin(segFPort,segFPin);
-
-		break;
-	case 21	:		//Shows - g
-	GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-		break;
-	case 22	:		//Shows o cdeg
-		GPIO_setOutputHighOnPin(segCPort,segCPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-		break;
-
-	case 23	:		//Shows raised o abfg
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-		break;
-	case 24:	//shows raised o with a underline abdfg
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-		break;
-	case 25: //Shows a _ d
-		GPIO_setOutputHighOnPin(segDPort,segDPin);
-		break;
-	case 26: //Shows nothing
-
-			break;
-	case 27: //Shows p abefg
-		GPIO_setOutputHighOnPin(segAPort,segAPin);
-		GPIO_setOutputHighOnPin(segBPort,segBPin);
-		GPIO_setOutputHighOnPin(segEPort,segEPin);
-		GPIO_setOutputHighOnPin(segFPort,segFPin);
-		GPIO_setOutputHighOnPin(segGPort,segGPin);
-
-
-			break;
-	}
-}
 
 //******************************************************************************
 //
